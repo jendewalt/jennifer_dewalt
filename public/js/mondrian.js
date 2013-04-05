0b35b2412a8e3d09ecaad7a1205c3827b0b66133
Jen Dewalt <jen.dewalt@gmail.com>
2013-04-04 18:25:22 -0700

    Fix bug in Mondrian

--- public/js/mondrian.js
+++ public/js/mondrian.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
 	var paint = "white"
 
 	$(".color").on("click", function () {
-		paint = $(this).css('background');
+		paint = $(this).css('background-color');
 		console.log(paint);
 	})
 
 	$('.row').on('click', function () {
 
-		$(this).css("background", paint );
+		$(this).css("background-color", paint );
 	});
 
 });
