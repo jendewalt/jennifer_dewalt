421a422a762f962c63ca2471e0d32084737a385d
Jen Dewalt <jen.dewalt@gmail.com>
2013-04-07 14:13:53 -0700

    Fix Day Tracker js

--- public/js/day_tracker.js
+++ public/js/day_tracker.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
 	var d = new Date();
-	var	day = d.getDate();
-	var	msg = ["",
+	var	day = d.getDay();
+	var	msg = ["Sunday Funday!",
 			   "Back to the grind!",
 			   "Oh. It's Tuesday.",
 			   "Boom! Wednesday!",
 			   "Thirsty Thursday, yo!",
 			   "It's Friday! We should be kickin' it!",
-			   "Paaarrrtttyyy!",
-			   "Sunday Funday!"]
+			   "Paaarrrtttyyy!"]
 
 	$('#' + day).attr('id', 'today');
 	$("#message").text(msg[day]);
