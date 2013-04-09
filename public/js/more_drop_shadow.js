388caaf9b02a8fb4c72c0104b2b2344420450159
Jen Dewalt <jen.dewalt@gmail.com>
2013-04-08 23:11:46 -0700

    fix More Drop Shadow

--- public/js/more_drop_shadow.js
+++ public/js/more_drop_shadow.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
 	$('.box_shadow').on('click', function () {
 
-		var current = $(event.target).css('box-shadow');
+		var current = $(this).css('box-shadow');
 
 		var a = current.split('px');
 		var blur = a[a.length-3];
@@ -20,34 +20,22 @@ $(document).ready(function () {
 		var newBlur = parseInt(blur) + 1; 
 		var newSpread = parseInt(spread) + 3; 
 		
-		$(event.target).css('box-shadow', 'rgba(-1, 1, 0, 0.35) 0px 0px ' + newBlur + 'px ' + newSpread + 'px');
+		$(this).css('box-shadow', 'rgba(-1, 1, 0, 0.35) 0px 0px ' + newBlur + 'px ' + newSpread + 'px');
 
 	});
 
 	$('.text_shadow').on('click', function () {
 
-		var current = $(event.target).css('text-shadow');
+		var current = $(this).css('text-shadow');
 
 		var a = current.split('px');
-		console.log(a);
-		
 		var blur = a[a.length-2];
-		console.log(blur);
-
 		var vert = a[a.length-3];
-		console.log(vert);
-		
-		// var horiz = a[a.length-4];
-		// console.log(horiz);
 
 		var newBlur = parseInt(blur) + 1; 
 		var newVert = parseInt(vert) + 1; 
-		// var newHoriz = parseInt(spread) + 3; 
-		
-		console.log(event.target);
-		console.log(current);
-		
-		$(event.target).css('text-shadow', 'rgb(-1, 1, 0) 1px ' + newVert + 'px ' + newBlur + 'px');
+			
+		$(this).css('text-shadow', 'rgb(-1, 1, 0) 1px ' + newVert + 'px ' + newBlur + 'px');
 
 	});
 
