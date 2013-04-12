d40390faa1a3745466b4e95ae8e7a86dbdf41351
Jen Dewalt <jen.dewalt@gmail.com>
2013-04-11 19:27:34 -0700

    Add Color Clock

---
+++ public/js/color_clock.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+	
+	function time() {
+		var now = new Date();
+		var hour =  now.getHours();
+		var min =  now.getMinutes();
+		var sec =  now.getSeconds();
+		
+		if (hour > 12) {
+			hour = hour - 12;
+		};
+
+		hour = formatTime(hour);
+		min = formatTime(min);
+		sec = formatTime(sec);
+
+		color = timeColor(hour, min, sec);
+
+		$('#cur_hour').text(hour);
+		$('#cur_min').text(min);
+		$('#cur_sec').text(sec);
+		$('body').css('background-color', '#' + color);
+		$('#cur_color').text(color);
+
+		t=setTimeout(function(){time()}, 500);
+	}
+
+	function formatTime(i) {
+		if (i < 10) {
+			i = '0' + i;
+		}
+		return i;
+	}
+	function formatColor(i) {
+		if (i.length < 2) {
+			i = '0' + i;
+		}
+		return i;
+	}
+
+	function timeColor(hour, min, sec) {
+
+		red = Math.round(255 * (hour / 23)).toString(16);
+		green = Math.round(255 * (min / 59)).toString(16);
+		blue = Math.round(255 * (sec / 59)).toString(16);
+
+		red = formatColor(red);
+		green = formatColor(green);
+		blue = formatColor(blue);
+
+		return (red + green + blue).toUpperCase();
+	}
+
+	time();	
+});
