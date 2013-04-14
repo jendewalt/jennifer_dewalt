f6005a64b01cfb2a85d2d5ec0fc5137e82577de6
Jen Dewalt <jen.dewalt@gmail.com>
2013-04-13 19:12:33 -0700

    Remove RPS from Home

--- public/js/rock_paper_scissors.js
+++ public/js/rock_paper_scissors.js
@@ -6,51 +6,50 @@ $(document).ready(function () {
 	var hands = [rock, paper, scissors]
 
 	$('.button').on('click', function () {
+		$('#right_hand').attr('src', "images/right_fist.png");
+		$('#left_hand').attr('src', "images/left_fist.png");
+
 		var userChoice = this.id;
 		var compChoice = hands[Math.floor(Math.random()*3)];
 
-
-		$('#right_hand').attr('src', "images/right_fist.png");
-		$('#left_hand').attr('src', "images/left_fist.png");
-		$('.hand_container').addClass('shake');
-		$('#score_container').text('3');
-		
-		countDown();
-
-		setTimeout(function() {
-
-			$('.hand_container').removeClass('shake');
-
-			$('#score_container').text('Shoot!');
+			$('#score_container').text('3');
+			$('.hand_container').addClass('shake');
 			
-			if (userChoice == "rock") {
-				userChoice = rock; 
-				$('#right_hand').attr('src', rock);
-			};
-			if (userChoice == "paper") {
-				userChoice = paper;
-				$('#right_hand').attr('src', paper);
-			};
-			if (userChoice == "scissors") {
-				userChoice = scissors;
-				$('#right_hand').attr('src', scissors);
-			};
-
-			$('#left_hand').attr('src', compChoice);
+			countDown();
 
 			setTimeout(function() {
-				if ( userChoice == compChoice ) {
-					$('#score_container').text('Tie!');
-				} else if ( userChoice == rock && compChoice == scissors || userChoice == paper && compChoice == rock || userChoice == scissors && compChoice == paper ) {
-					$('#score_container').text('You Win!');
-				} else {
-					$('#score_container').text('You Lose!');
+
+				$('.hand_container').removeClass('shake');
+
+				$('#score_container').text('Shoot!');
+				
+				if (userChoice == "rock") {
+					userChoice = rock; 
+					$('#right_hand').attr('src', rock);
+				};
+				if (userChoice == "paper") {
+					userChoice = paper;
+					$('#right_hand').attr('src', paper);
+				};
+				if (userChoice == "scissors") {
+					userChoice = scissors;
+					$('#right_hand').attr('src', scissors);
 				};
 
-			}, 400)
+				$('#left_hand').attr('src', compChoice);
 
+				setTimeout(function() {
+					if ( userChoice == compChoice ) {
+						$('#score_container').text('Tie!');
+					} else if ( userChoice == rock && compChoice == scissors || userChoice == paper && compChoice == rock || userChoice == scissors && compChoice == paper ) {
+						$('#score_container').text('You Win!');
+					} else {
+						$('#score_container').text('You Lose!');
+					};
 
-		}, 1500);
+				}, 400)
+
+			}, 1500);
 
 	});
 
