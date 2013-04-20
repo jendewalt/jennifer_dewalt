7b9000e8f3a62b7457cc49302dd36a319cf7adcb
Jen Dewalt <jen.dewalt@gmail.com>
2013-04-19 23:31:36 -0700

    Add Hipster Quiz

---
+++ public/js/hipster_quiz.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+	$('form').on('submit', function (event) {
+		event.preventDefault();
+
+		var total = getScore();
+		var message;
+		
+		if (total < 52*1/4) {
+			message = "You're definitely not an SF hipster.";
+		} else if (total < 52*1/2) {
+			message = "You might be an SF hipster.";
+		} else if (total < 52*3/4) {
+			message = "You're most likely an SF hipster.";
+		} else {
+			message = "You're a Super SF Hipster!";
+		}
+
+		var your_score = "<div id='score'>You're score is " + total + '.</div>';
+		message = '<div id="message">' + message + '</div>';
+
+		$('#container').html(your_score + message);
+
+		function getScore() {
+			var score = 0;
+
+			for (var i = 1; i < 15; i++) {
+				var question = i;
+
+				var answer = $('input[name=q' + i + ']:checked').val();
+
+				if (answer) {
+					score += parseInt(answer);
+				}
+			}
+			return score;
+		};
+	});
+});
