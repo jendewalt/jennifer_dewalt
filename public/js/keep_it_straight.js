c8a385ccf926591e3a0b786a5ad7fdacf074ad84
Jen Dewalt <jen.dewalt@gmail.com>
2013-04-22 19:04:40 -0700

    Add Keep It Straight

---
+++ public/js/keep_it_straight.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+	$('.start').on('mouseenter', function () {
+		var win = false;
+		var	failPhrases = ["That's lame.",
+					 	   "Ouch, too bad.",
+					 	   "I am sure you're good at something.",
+					 	   "That wasn't too pretty.",
+					 	   "Well, at least you tried.",
+					 	   "Maybe cursors just aren't your thing.",
+					 	   "I've seen worse failures.",
+					 	   "Well that sucked.",
+					 	   "Are you using your foot?"]
+
+		$('.end').on('mouseenter', function () {
+			$('#modal').show();
+			win = true;
+			console.log('win');
+		});
+
+		$('.game_container').on('mouseleave', function () {
+			if (win == false) {
+				var phrase = failPhrases[Math.floor(Math.random() * 9)];
+
+				$('#modal').html("<h1>FAIL!</h1>" + "<h2>" + phrase + "</h2>" + "<a href='' class='button' id='try_again'>Try Again?</a>" + "<a href='keep_it_straight.html' class='button'>Start Over</a>").show();
+
+				$('#try_again').on('click', function (e) {
+					e.preventDefault();
+					location.reload();
+				});
+			}
+		});
+
+	});
+});
