ed9a9865c08528664dac0fe7c37716a278159f1d
Jen Dewalt <jen.dewalt@gmail.com>
2013-04-24 18:27:42 -0700

    Add Email Impact

---
+++ public/js/email_impact.js
@@ -0,0 +1,35 @@
+$(document).ready(function (){
+	$('textarea').focus();
+
+	$('body').on('click', function () {
+		$('textarea').focus();
+	});
+
+	$('textarea').one('keypress', function () {
+		$('#prompt_container').fadeOut('slow');
+		$('#info_container').fadeIn('slow');
+	});
+
+	$('textarea').on('keyup', function () {
+		var text = $('textarea').val();
+		var size = 130*(Math.pow(Math.pow(1/13, (1/250)), text.length));
+
+		$('textarea').css('font-size', size + 'px');
+		$('#char_count').text('Character Count: ' + text.length)
+
+		if (text.length >= 150) {
+			$('#info_container').css('background-color', '#f2ad00');
+		} 
+		if (text.length >= 200) {
+			$('#info_container').css('background-color', '#ed6700');
+		} 
+		if (text.length >= 250) {
+			$('#info_container').css('background-color', '#ba000d');
+		} 
+		if (text.length < 150) {
+			$('#info_container').css('background-color', '#42863e');
+		}
+	});
+});
+
+
