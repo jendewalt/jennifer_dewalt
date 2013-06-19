348ca81224bd01beb53dd8745e6433bb5c8c21ae
Jen Dewalt <jen.dewalt@gmail.com>
2013-06-18 23:09:30 -0700

    Add PV Calc

--- app/assets/javascripts/websites/pv_calculator_calculator.js
+++ app/assets/javascripts/websites/pv_calculator_calculator.js
@@ -12,16 +12,71 @@ function pvCalculator() {
 	$('#basic_years_btn').on('click', function (e) {
 		e.preventDefault();
 		var num_years = $('#basic_years').val();
-		if ($.isNumeric(num_years)) {
-			addRevenueFields(num_years);
-		} else {
-			$('#basic_years').val('');
-			alert('Number of years must be a number.');
+		if (!$.isNumeric(num_years)) {
+			$('#basic_years').val('10');
+			num_years = 10;
 		}
+		
+		addRevenueFields(num_years);
+	});
+
+	$('#basic_calc form').on('change', function (e) {
+		e.preventDefault();
+		calculateBasic();		
 	});
 
 	$('#basic_calc form').on('submit', function (e) {
 		e.preventDefault();
+		calculateBasic();		
+	});
+
+	$('#compounding_calc form').on('submit', function (e) {
+		e.preventDefault();
+		calculateCompound();						
+	});
+
+	$('#compounding_calc form').on('change', function (e) {
+		e.preventDefault();
+		calculateCompound();						
+	});
+
+	$('#offset_calc form').on('submit', function (e) {
+		e.preventDefault();
+		calculateOffset();					
+	});
+
+	$('#offset_calc form').on('change', function (e) {
+		e.preventDefault();
+		calculateOffset();					
+	});
+
+	function calculateOffset() {
+		var r = parseFloat($('#offset_calc .int_rate').val()),
+			y = parseFloat($('#offset_calc .num_years').val()),
+			x = parseFloat($('#offset_calc .num_years_no_rev').val()),
+			c = parseFloat($('#offset_calc .revenue').val()),
+			g = parseFloat($('#offset_calc .growth').val()),
+			t = y - x,
+			answer = 0;
+
+		answer = (c / (r - g)) - (c / (r - g) * Math.pow((1 + g), t) / Math.pow((1 + r), t)) / Math.pow((1+r), x);
+
+		updateAnswer(answer, '#offset_calc');
+	};
+
+	function calculateCompound() {
+		var r = parseFloat($('#compounding_calc .int_rate').val()),
+			t = parseFloat($('#compounding_calc .num_years').val()),
+			c = parseFloat($('#compounding_calc .revenue').val()),
+			g = parseFloat($('#compounding_calc .growth').val()),
+			answer = 0;
+
+		answer = (c / (r - g)) - (c / (r - g)) * (Math.pow((1 + g), t) / Math.pow((1 + r), t));
+
+		updateAnswer(answer, '#compounding_calc');
+	};
+
+	function calculateBasic() {
 		var r = parseFloat($('#basic_calc .int_rate').val()),
 			revs = $('.year_rev input'),
 			answer = 0;
@@ -32,36 +87,8 @@ function pvCalculator() {
 			answer = answer + (c / (Math.pow((1 + r), i+1)));
 		});
 
-		updateAnswer(answer, '#basic_calc');			
-	});
-
-	$('#compounding_calc form').on('submit', function (e) {
-		e.preventDefault();
-		var r = parseFloat($('#compounding_calc .int_rate').val()),
-			t = parseFloat($('#compounding_calc .num_years').val()),
-			c = parseFloat($('#compounding_calc .revenue').val()),
-			g = parseFloat($('#compounding_calc .growth').val()),
-			answer = 0;
-
-		answer = (c / (r - g)) - (c / (r - g)) * (Math.pow((1 + g), t) / Math.pow((1 + r), t));
-
-		updateAnswer(answer, '#compounding_calc');						
-	});
-
-	$('#offset_calc form').on('submit', function (e) {
-		e.preventDefault();
-		var r = parseFloat($('#offset_calc .int_rate').val()),
-			y = parseFloat($('#offset_calc .num_years').val()),
-			x = parseFloat($('#offset_calc .num_years_no_rev').val()),
-			c = parseFloat($('#offset_calc .revenue').val()),
-			g = parseFloat($('#offset_calc .growth').val()),
-			t = y - x;
-			answer = 0;
-
-		answer = (c / (r - g)) - (c / (r - g)) * Math.pow((1 + g), t) / Math.pow((1 + r), t) / Math.pow((1+r), x);
-
-		updateAnswer(answer, '#offset_calc');						
-	});
+		updateAnswer(answer, '#basic_calc');
+	};
 
 	function updateAnswer(ans, id) {
 		if ($.isNumeric(ans)) {
