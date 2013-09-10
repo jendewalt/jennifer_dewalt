6f07554ac8fb0aec687fda0a36a1b9c269183065
Jen Dewalt <jen.dewalt@gmail.com>
2013-09-09 21:36:03 -0700

    Precompile assets

--- app/controllers/window/page_controller.rb
+++ app/controllers/window/page_controller.rb
@@ -7,19 +7,9 @@ class Window::PageController < ApplicationController
     photos = flickr.photos.search(:tags => 'nature, sky, field, trees', :content_type => 1, :extras => 'url_l')
 
     photos.each do |image|
-      # farm_id = image['farm'].to_s
-      # server_id = image['server'].to_s
-      # photo_id = image['id'].to_s
-      # secret = image['secret'].to_s
-
       if image['url_l'] and image['height_l'].to_i > 550
         @img_urls.push(image['url_l'])
       end
     end
-
-    logger.debug('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    logger.debug(photos.inspect)
-    logger.debug('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    logger.debug(@img_urls)
   end
 end
