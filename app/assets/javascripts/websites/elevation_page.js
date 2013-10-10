5b4ce91105207940fed90728aab5b9738c569c1c
Jen Dewalt <jen.dewalt@gmail.com>
2013-10-09 20:40:55 -0700

    Update Geocoder Elevations

--- app/assets/javascripts/websites/elevation_page.js
+++ app/assets/javascripts/websites/elevation_page.js
@@ -52,7 +52,7 @@ function elevationPage() {
 		_.each(locations, function (loc, i) {
 			geocoder.geocode( {'address': loc}, function (results, status) {
 				if (status == google.maps.GeocoderStatus.OK) {
-					var latlng = new google.maps.LatLng(results[0].geometry.location.mb, results[0].geometry.location.nb);
+					var latlng = new google.maps.LatLng(results[0].geometry.location.lb, results[0].geometry.location.mb);
 					path.push(latlng);
 
 					if (i == 0){
